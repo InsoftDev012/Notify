f65ada3ab383fa651a8e1dedfa9bb65c97ce7142
AnkitInsoft <ankit@insoftnepal.com>
2024-10-20 16:03:55 +0545

    :feat: used tailnext to create starting template - https://tailnext.vercel.app/

--- notify.client/next.config.mjs
+++ notify.client/next.config.mjs
@@ -1,22 +1,42 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    webpack: (config) => {
-        config.resolve.alias["@"] = "./src";
-        return config;
-    },
-    rewrites: async () => {
-        const target = process.env.ASPNETCORE_HTTPS_PORT
-            ? `https://localhost:${process.env.ASPNETCORE_HTTPS_PORT}`
-            : process.env.ASPNETCORE_URLS?.split(";")[0] || "https://localhost:7123";
 
-        return [
-            {
-                source: "/weatherforecast",
-                destination: `${target}/weatherforecast`,
-            },
-        ];
-    },
+import { SITE } from "./src/config.js";
+
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias["@"] = "./src";
+    return config;
+  },
+  rewrites: async () => {
+    const target = process.env.ASPNETCORE_HTTPS_PORT
+      ? `https://localhost:${process.env.ASPNETCORE_HTTPS_PORT}`
+      : process.env.ASPNETCORE_URLS?.split(";")[0] || "https://localhost:7123";
+
+    return [
+      {
+        source: "/weatherforecast",
+        destination: `${target}/weatherforecast`,
+      },
+    ];
+  },
+  trailingSlash: SITE.trailingSlash,
+  basePath: SITE.basePathname !== "/" ? SITE.basePathname : "",
+
+  swcMinify: true,
+  poweredByHeader: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- notify.client/src/app/page.tsx
+++ notify.client/src/app/page.tsx
@@ -1,9 +1,52 @@
+import type { Metadata } from "next";
 
+import { SITE } from "@/src/config.js";
+
+import Hero from "@/src/components/widgets/Hero";
+import SocialProof from "@/src/components/widgets/SocialProof";
+import Features from "@/src/components/widgets/Features";
+import Content from "@/src/components/widgets/Content";
+import Steps from "@/src/components/widgets/Steps";
+import Testimonials from "@/src/components/widgets/Testimonials";
+import FAQs2 from "@/src/components/widgets/FAQs2";
+import Pricing from "@/src/components/widgets/Pricing";
+import Team from "@/src/components/widgets/Team";
+import CallToAction2 from "@/src/components/widgets/CallToAction2";
+import Contact from "@/src/components/widgets/Contact";
+import {
+  callToAction2Home,
+  contactHome,
+  contentHomeOne,
+  contentHomeTwo,
+  faqs2Home,
+  featuresHome,
+  heroHome,
+  pricingHome,
+  socialProofHome,
+  stepsHome,
+  teamHome,
+  testimonialsHome,
+} from "@/src/shared/data/pages/home.data";
+
+export const metadata: Metadata = {
+  title: SITE.title,
+};
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <Hero {...heroHome} />
+      <SocialProof {...socialProofHome} />
+      <Features {...featuresHome} />
+      <Content {...contentHomeOne} />
+      <Content {...contentHomeTwo} />
+      <Steps {...stepsHome} />
+      <Testimonials {...testimonialsHome} />
+      <FAQs2 {...faqs2Home} />
+      <Pricing {...pricingHome} />
+      <Team {...teamHome} />
+      <Contact {...contactHome} />
+      <CallToAction2 {...callToAction2Home} />
     </>
   );
 }
